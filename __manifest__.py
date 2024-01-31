e25b6bc4c6b19a4fb65982d06eefd23790df46f8
pulpiicifpufmoll <dacedopulpillo@cifpfbmoll.eu>
2024-01-31 15:59:15 +0100

    hol

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Gestion de biblioteca",
-    'summary': "Gestión de biblioteca",
-    'author': "David Acedo Pulpillo",
-    'license': "AGPL-3",
-    'website': "",
-    'version': '1.0',
-    'depends': ['base'],
-    'application': True,
-    'category': "Services/Library",
+    'name': "Gestión de la biblioteca",
 
-    "data": [
-        "security/ir.model.access.csv",
+    'summary': """
+        Gestión del catálogo de la biblioteca y los préstamos de libros
+        """,
+
+
+    'author': "Emilio Real",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Services/Library',
+    'version': '15.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    'data': [
         "security/library_security.xml",
+        "security/ir.model.access.csv",
         "views/book_view.xml",
-        "views/book_list_template.xml"
         "views/library_menu.xml",
-    ]
+        "views/book_list_template.xml",
+    ],
+
+    'application': True,
 }
--- models/library_book.py
+++ models/library_book.py
@@ -1,34 +1,36 @@
-from odoo import fields, models
+from odoo import models, fields
 from odoo.exceptions import ValidationError
-from odoo.exceptions import UserError
 
-
-class book(models.Model):
+class Book(models.Model):
     _name = "library.book"
     _description = "Book"
 
-    name = fields.Char("Titulo", required=True)
+    name = fields.Char("Título", required=True)
     isbn = fields.Char("ISBN")
     active = fields.Boolean("Activo?", default=True)
-    date_published = fields.Date("Dia publicado")
-    image = fields.Binary("Portada")
+    date_published = fields.Date()
+    image = fields.Binary("Cubierta")
     publisher_id = fields.Many2one("res.partner", string="Editorial")
-    author_ids = fields.Many2many("res.partner", string="Autor")
-    
+    author_ids = fields.Many2many("res.partner", string="Autores")
+
     def _check_isbn(self):
         self.ensure_one()
         digits = [int(x) for x in self.isbn if x.isdigit()]
-        if len(digits) == 13:
-            ponderations = [1, 3] * 6
-            terms = [a * b for a, b in zip(digits[:12], ponderations)]
-            remain = sum(terms) % 10
+        if (len(digits)==13):
+            poderations = [1,3]*6
+            terms=[a*b for a,b in zip(digits[:12],poderations)]
+            remain = sum(terms)%10
             check = 10 - remain if remain != 0 else 0
             return digits[-1] == check
-
+    
     def button_check_isbn(self):
         for book in self:
             if not book.isbn:
-                raise ValidationError("Por favor introduce un ISBN para %s" % book.name)
+                raise ValidationError("Por favor, proporciona un ISBN para %s" 
+                    %book.name)
             if book.isbn and not book._check_isbn():
-                raise ValidationError("%s ISBN es invalido" % book.isbn)
+                raise ValidationError("El ISBN %s no es válido" 
+                %book.isbn)
         return True
+
+    
